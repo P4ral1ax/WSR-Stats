1973f881757b834fbd336c7de3a19a1548dd23fa
P4ral1ax <braydenjw00@gmail.com>
2022-06-10 09:09:58 -0400

    Heroku Pray it works

--- get_cookie.sh
+++ get_cookie.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo $email
 curl -c cookie-jar.txt -X POST -H 'Content-Type: application/json' --data "{\"email\": \"$email\", \"password\": \"$password\"}" https://members-ng.iracing.com/auth
