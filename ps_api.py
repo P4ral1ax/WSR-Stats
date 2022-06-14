17f93293f62f135c854ff02beeff0e668405ab7d
P4ral1ax <braydenjw00@gmail.com>
2022-06-14 18:52:02 -0400

    Gunicorn Test

--- ps_api.py
+++ ps_api.py
@@ -82,6 +82,6 @@ api.add_resource(sessionStats, '/stats')
 api.add_resource(sessionInfo, '/info')
 
 if __name__ == '__main__':
-    flask_port = os.getenv('PORT')
-    app.run(port=flask_port, host="0.0.0.0")
-    # app.run(debug=True)
+    #iflask_port = os.getenv('PORT')
+    #app.run(port=flask_port, host="0.0.0.0")
+    app.run()
